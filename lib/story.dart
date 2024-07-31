1d1019c575313d6c94b71ca0d0068eba00fbb15f
ali-ab-2003 <ali.abdulllah.003@gmail.com>
2024-07-31 06:45:48 +0500

    final app

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story {
+  // Properties
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  // Constructor
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
+}
